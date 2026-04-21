4f0244862364729f1ca9d265c2b2560e34ee4d88
Umar Draz <umardraz1991@gmail.com>
2026-04-21 01:58:13 +0100

    fix cors for azure

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,12 @@ const path = require("path");
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"]
+}));
+app.options("*", cors());
 app.use(express.json());
 
 // Connect MongoDB
